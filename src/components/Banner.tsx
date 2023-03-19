9d57e54e29131f86813e19017384ce3261e06b37
sho40 <kanazawa.sho40@gmail.com>
2023-03-19 19:38:59 +0900

    バナーサイズ調整

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 
 export const Banner = () => {
   return (
-    <div className="px-4 w-screen h-10 bg-gray-400 text-center leading-10">
+    <div className="px-4 w-screen bg-gray-400 text-center" style={{height: "60px", lineHeight: "60px"}}>
       <button onClick={() => {"詳細ページへ移動"}}>
         <div className="text-white">CINCLOの使い方バナー</div>
       </button>
