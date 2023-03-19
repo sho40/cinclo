181ce347bfd2492540d0ead383f16247a010e718
sho40 <kanazawa.sho40@gmail.com>
2023-03-19 18:58:02 +0900

    fontawesome導入、ヘッダー仮実装

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import {
+  faBars,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faHeart
+} from "@fortawesome/free-regular-svg-icons"
+import { IconButton } from '@/utils/IconButton';
+
+export const Header = () => {
+  return (
+    <div className='flex justify-between px-3 py-2 relative'>
+      <div>
+        <IconButton onClick={() => {console.log("clicked")}} icon={faBars}/>
+      </div>
+      {/* tailwindで実現できなかったため直でCSS指定 */}
+      <div style={{position: "absolute", left: "50%", transform: "translateX(-50%)"}}>
+        CINCLO
+      </div>
+      <div className='flex'>
+        <div className='pr-2'>
+          <IconButton onClick={() => {console.log("clicked")}} icon={faHeart}/>
+        </div>
+        <div>
+          <IconButton onClick={() => {console.log("clicked")}} icon={faCartShopping}/>
+        </div>
+      </div>
+    </div>
+  )
+}
