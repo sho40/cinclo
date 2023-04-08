93486730c22785d8c49e6b12f6c11c5275a88ad3
sho40 <kanazawa.sho40@gmail.com>
2023-04-08 23:57:23 +0900

    サイドメニューにリンクを追加

--- src/components/admin/sideMenu/SideMenuContainer.tsx
+++ src/components/admin/sideMenu/SideMenuContainer.tsx
@@ -5,9 +5,9 @@ export default function SideMenuContainer() {
   return (
     <div className="w-1/5 min-h-full bg-gray-700 text-white" style={{minWidth: "270px"}}>
       <SideMenuHeader />
-      <SideMenuButton title="ダッシュボード" onClick={() => {}} selected={false}/>
-      <SideMenuButton title="アイテム一覧" onClick={() => {}} selected={false}/>
-      <SideMenuButton title="注文管理" onClick={() => {}} selected={false}/>
+      <SideMenuButton title="ダッシュボード" url="/admin" selected={false}/>
+      <SideMenuButton title="アイテム一覧" url="/admin/item-list" selected={false}/>
+      <SideMenuButton title="注文管理" url="/admin" selected={false}/>
     </div>
   )
 }
