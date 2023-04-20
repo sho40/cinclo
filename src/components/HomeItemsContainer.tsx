140a454f9633364c3c4ae43d78c4f10de50db59b
sho40 <kanazawa.sho40@gmail.com>
2023-04-21 03:01:28 +0900

    homeにおすすめ商品を表示する

--- src/components/HomeItemsContainer.tsx
+++ src/components/HomeItemsContainer.tsx
@@ -1,39 +1,64 @@
+import { GetRecommendedItemsForHomeQuery } from "@/libs/apollo/graphql";
+import { isValidUrl } from "@/logic/checkUrl";
 import { numberToPrice } from "@/logic/numberFormatter";
-import { Item } from "@/types/Item.type";
+import Image from 'next/image';
 import Link from "next/link";
+import React from "react";
+
+interface Items {
+  items: GetRecommendedItemsForHomeQuery["items"]
+}
 
 export const HomeItemsContainer = (props: {
   title: string;
-  items: Item[];
+  items: GetRecommendedItemsForHomeQuery["items"] | undefined;
 }) => {
+
   return(
-    <div>
-      <div className='pb-3 text-base'>{props.title}</div>
-      <ul className='flex flex-wrap justify-between text-sm'>
-        {props.items.map((item, index) => {
-          return(
-            <li key={index} className='mb-7' style={{ width: 'calc((100% - 16px) / 2)' }}>
-              {/* TODO リンクを指定する */}
-              <Link href='/'>
-                {/* TODO 画像を表示する */}
-                <div className='bg-gray-400 mb-1' style={{height: '190px'}}></div>
-                <p>{item.brand}</p>
-                <div>
-                  <span className='mr-4'><del>{numberToPrice(item.basePrice)}</del></span>
-                  <span className='text-red-400 font-bold'>{numberToPrice(item.currentPrice)}</span>
-                </div>
-                <p>{`貸出回数 ${item.rentalCount} 回`}</p>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-      <div className='text-center'>
-        {/* TODO リンクにする */}
-        <button>
-          <div className='rounded-none px-2 py-1' style={{border: "1px solid #111"}}>もっと見る</div>
-        </button>
-      </div>
-    </div>
+    <>
+      {
+        props.items == null ? (
+          <></>
+        ) : (
+          <div>
+            <div className='pb-3 text-base'>{props.title}</div>
+            <ul className='flex flex-wrap justify-between text-sm'>
+              {props.items.map((item, index) => {
+                return(
+                  <li key={index} className='mb-7' style={{ width: 'calc((100% - 16px) / 2)' }}>
+                    {/* TODO リンクを指定する */}
+                    <Link href='/'>
+                      {/* TODO 画像を表示する */}
+                      <div>
+                        {item.images != null && item.images.length === 1 && isValidUrl(item.images[0].url) ?
+                          // <div className={styles.imageContainer}>
+                          <div style={{position: "relative", height: '190px', objectFit: 'cover'}}>
+                            <Image src={item.images[0].url} alt="" fill/>
+                          </div> 
+                          :
+                          <div className='bg-gray-400 mb-1' style={{height: '190px'}} />
+                        }
+                      </div>
+                      <p>{item.name}</p>
+                      <div>
+                        <span className='text-red-400 font-bold mr-4'>{item.current_price != null ? numberToPrice(item.current_price) : ""}</span>
+                        <span><del>{item.regular_price != null ? numberToPrice(item.regular_price) : ""}</del></span>
+                      </div>
+                      <p>{`貸出回数 ${item.current_count} 回`}</p>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+            <div className='text-center'>
+              {/* TODO リンクにする */}
+              <button>
+                <div className='rounded-none px-2 py-1' style={{border: "1px solid #111"}}>もっと見る</div>
+              </button>
+            </div>
+          </div>
+        )
+      }
+    </>
   )
 }
