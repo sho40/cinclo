667bfb434a69e939f651a609aa074af8aaece80a
sho40 <kanazawa.sho40@gmail.com>
2023-06-07 02:45:17 +0900

    一覧画面に全商品出す

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import { HomeItemsContainer } from '@/components/HomeItemsContainer'
 import { SlideBunner } from '@/components/slideanner/SlideBanner'
 
 import { gql } from '@apollo/client'
-import { useGetRecommendedItemsForHomeQuery, useGetNewArrivalItemsQuery } from '../libs/apollo/graphql'
+import { useGetRecommendedItemsForHomeQuery, useGetNewArrivalItemsQuery, useGetAllItemCountQuery, useGetAllItemListQuery } from '../libs/apollo/graphql'
 import { Layout } from '@/components/customer/Layout'
+import { ItemCard } from '@/components/customer/item-card/ItemCard'
+import styles from "./home.module.scss"
 
 export default function Home() {
   // MEMO _in === gender
@@ -12,11 +14,14 @@ export default function Home() {
       <Layout>
       {/* <GenderSwitch /> */}
       <SlideBunner />
-      <div className='px-2'>
+      {/* <div className='px-2'>
         <Recommend />
       </div>
       <div className='px-2'>
         <NewArrival />
+      </div> */}
+      <div>
+        <AllItems />
       </div>
       </Layout>
     </>
@@ -44,23 +49,52 @@ const GenderSwitch = () => {
     </div>
   )
 }
+// TODO: とりあえず一覧画面には全商品出す
 
-const Recommend = () => {
-  const { data } = useGetRecommendedItemsForHomeQuery({variables : {limit: 6, _in: [1, 2, 3]}});
+// const Recommend = () => {
+//   const { data } = useGetRecommendedItemsForHomeQuery({variables : {limit: 6, _in: [1, 2, 3]}});
 
-  return(
-    <div className='py-7'>
-      <HomeItemsContainer items={data?.items} title='Recommend'/>
-    </div>
-  )
-}
+//   return(
+//     <div className='py-7'>
+//       <HomeItemsContainer items={data?.items} title='Recommend'/>
+//     </div>
+//   )
+// }
 
-const NewArrival = () => {
-  const { data } = useGetNewArrivalItemsQuery({variables : {limit: 6, _in: [1, 2, 3]}});
+// const NewArrival = () => {
+//   const { data } = useGetNewArrivalItemsQuery({variables : {limit: 6, _in: [1, 2, 3]}});
+
+//   return (
+//     <div className='py-7'>
+//       <HomeItemsContainer items={data?.items} title='New Arrival'/>
+//     </div>
+//   )
+// }
+
+const AllItems = () => {
+  const itemCount = useGetAllItemCountQuery();
+  const allItemCount: number = itemCount.data?.items_aggregate.aggregate?.count ?? 0;
+
+  const { data, loading } = useGetAllItemListQuery();
 
   return (
     <div className='py-7'>
-      <HomeItemsContainer items={data?.items} title='New Arrival'/>
+      <div className={styles.title}>
+        <h1>
+          {`ALL ITEMS`}
+        </h1>
+      </div>
+      <div className={styles.container}>
+        {loading ? <>loading ...</> : (
+          <ul className='flex flex-wrap justify-between text-sm'>
+            {data?.items.map((item, index) => 
+              <li key={index} className='mb-7' style={{ width: 'calc((100% - 16px) / 2)' }}>
+                <ItemCard item={item}/>
+              </li>
+            )}
+        </ul>
+        )}
+      </div>
     </div>
   )
 }
