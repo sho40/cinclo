86b919c4a6dcf4f11cd7f5a384e14c8cce24022e
sho40 <kanazawa.sho40@gmail.com>
2023-06-11 14:41:23 +0900

    apikeyを適用

--- src/pages/api/sendGrid.ts
+++ src/pages/api/sendGrid.ts
@@ -6,12 +6,11 @@ export default async function handler(req: any, res: any) {
 
   // FIXME: 本文の内容確認する
   const sgMail = require('@sendgrid/mail');
-  // TODO: APIKey取得し定義
-  // sgMail.setApiKey(process.env.SENDGRID_APIKEY);
+  sgMail.setApiKey(process.env.SENDGRID_APIKEY);
   const msg = {
     to: req.body.email,
     bcc: '',
-    from: '',
+    from: 'order-noreply@cinclo.jp',
     subject: 'お買い上げ明細をお届けいたします。',
     text: `このメールはCINCLOから自動で送信しています\n${req.body.name} 様\nお買い上げ明細をお届けいたします。\nお買い上げ商品\n${req.body.itemNames.map((itemName: string) => `${itemName}\n`)}\n\nご購入日時\n${req.body.date}\n到着予定日\n${req.body.arraivalDate}\nお支払い合計金額\n${req.body.totalAmount}\n購入のキャンセルや返金はできません。\nご返却予定日\n${req.body.returnDate}\nご返却予定日を超えますと延滞金 購入単価x10%/日 発生しますのでご承知置きください。\n返送時は商品を再度梱包の上、同梱された発送伝票をご使用し返送お願いいたします。\n詳細は以下のページをご覧ください\nhttps---\nこのメールアドレスには返信できません。\n\nSELECT SHOP SoLuNa`
   }
