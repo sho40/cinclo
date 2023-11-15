19a98567a6b6be7fca08b1b06b0ec4448d069bcf
sho40 <kanazawa.sho40@gmail.com>
2023-11-16 01:22:29 +0900

    google search consoleのmetaタグを設置

--- src/components/meta/Meta.tsx
+++ src/components/meta/Meta.tsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 export const Meta = () => {
   return (
     <Head>
-      <meta name="robots" content="noindex,nofollow"/>
+      <meta name="google-site-verification" content="g-Ec0RaQQ8Ot0CtOgjVX4G7GsygzL-MXjSxj8lQ2I3s" />
     </Head>
   )
 }
